4604e9802f0325948ddf04e0b38e292c001d1814
aidanliddiard <aidanliddiard@gmail.com>
2022-03-24 15:00:58 -0700

    players displaying on the page

--- src/Views/Players.js
+++ src/Views/Players.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchPlayers } from '../services/players';
 
 export default function Players() {
+  const [players, setPlayers] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchPlayers();
       console.log(data);
+      setPlayers(data);
     };
     fetchData();
   }, []);
-  return <div>Players</div>;
+  return (
+    <div>
+      <h2>List of Players</h2>
+      <ul>
+        {players.map((player) => (
+          <li key={player.id}>{player.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
