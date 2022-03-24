fe850448e5fd3c33a008deda3732be22e018423c
aidanliddiard <aidanliddiard@gmail.com>
2022-03-23 17:56:06 -0700

    teams displaying on the page

--- src/Views/Teams.js
+++ src/Views/Teams.js
@@ -1,14 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchTeams } from '../services/teams';
 
 export default function Teams() {
+  const [teams, setTeams] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchTeams();
-      console.log(data);
+      setTeams(data);
     };
     fetchData();
   }, []);
 
-  return <div>Teams</div>;
+  return (
+    <div>
+      <h3>Teams</h3>
+      <ul>
+        {teams.map((team) => (
+          <li key={team.id}>{team.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
